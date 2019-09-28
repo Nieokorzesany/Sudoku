192a266c80aff5949bdb658aaea2a7779081e481
Piotr Wlazeł <pancu1222@gmail.com>
2019-09-28 12:02:09 +0200

    add initial project structure

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Tile from "./Components/Tile";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Tile />
+      </div>
+    );
+  }
+}
+
+export default App;

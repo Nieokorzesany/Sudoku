06587fda054c1490d821bca1ee85e9e7f197cd5d
Piotr Wlazeł <pancu1222@gmail.com>
2019-10-02 00:27:12 +0200

    Updates

---
+++ precache-manifest.f9562898a6d07e50a6cdb7b5cab6cd4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8974b2ba1318ad773d37ac028fe21ce",
+    "url": "/sudoku/index.html"
+  },
+  {
+    "revision": "49b9c5bf43db99f37959",
+    "url": "/sudoku/static/css/main.aadd284c.chunk.css"
+  },
+  {
+    "revision": "ee7dac98d0e224b19f5a",
+    "url": "/sudoku/static/js/2.72894d6d.chunk.js"
+  },
+  {
+    "revision": "49b9c5bf43db99f37959",
+    "url": "/sudoku/static/js/main.3a497c34.chunk.js"
+  },
+  {
+    "revision": "02f4d52b9c81bf08a2aa",
+    "url": "/sudoku/static/js/runtime-main.a200b866.js"
+  }
+]);
